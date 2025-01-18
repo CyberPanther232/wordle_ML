cd66ec37b454801e5d7927a9e0a35e8b32401b38
CyberPanther232 <kinneyh27@gmail.com>
2025-01-18 15:08:28 -0500

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	deleted:    .wordlistsprior_wordle_list_chrono.txt
    	modified:   update_list.py
    	modified:   wordlists/prior_wordle_list_chrono.txt

--- update_list.py
+++ update_list.py
@@ -5,4 +5,4 @@ WORDLIST_LINK = "https://eagerterrier.github.io/previous-wordle-words/chronologi
 
 print("Running chronological wordlist update...")
 os.system(f"curl -O -s {WORDLIST_LINK}")
-os.system("mv chronological.txt .\\wordlists\\prior_wordle_list_chrono.txt")
+os.system(r"mv chronological.txt ./wordlists/prior_wordle_list_chrono.txt")
